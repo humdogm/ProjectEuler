7fd89544791e009869aef5a33e511bf8a939056f
Humza <humziq@gmail.com>
2021-02-02 01:41:15 -0600

    Rewrote to be more flexible in use

--- solution.py
+++ solution.py
@@ -1,4 +1,9 @@
-threes = [ i for i in range(3, 1000, 3) ]
-fives = [ i for i in range(5, 1000, 5) ]
-s = sum(threes) + sum(fives)
-print(s)
+def sum_multiples(multiples, limit):
+    s = 0
+    for m in multiples:
+        s += sum(range(m, limit, m))
+    return s
+
+result = sum_multiples([3,5], 1000)
+print(result)
+
